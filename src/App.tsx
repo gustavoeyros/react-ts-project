f44fd406b508b7b9d3ed28038c2ed70958384abe
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-01-24 17:06:06 -0300

    feat: working with props + typescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Todos from './components/Todos'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <Todos items={['Learn React', 'Learn TypeScript']} />
+  )
+}
+
+export default App
