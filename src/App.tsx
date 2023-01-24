f261c8cf2802d7c7472ca16fb5737bce505c16d0
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-01-24 17:44:43 -0300

    feat: style added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
+import { useState } from 'react'
+import NewTodo from './components/NewTodo'
 import Todo from './models/todo'
 import Todos from './components/Todos'
 import './App.css'
 
 function App() {
-  const todos = [
-      new Todo('Learn React'),
-      new Todo('Learn Typescript')
-    ]
+  const [todos, setTodos] = useState<Todo[]>([])
   
+    const addTodoHandler = (todoText: string) => {
+      const newTodo = new Todo(todoText)
+
+      setTodos((prevTodos) => {
+        return prevTodos.concat(newTodo)
+      })
+    }
 
   return (
+    <>
+    <NewTodo onAddTodo={addTodoHandler} />
     <Todos items={todos} />
+    </>
   )
 }
 
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,8 +1,9 @@
+import classes from './Todos.module.css'
 import Todo from "../models/todo";
 import TodoItem from "./TodoItem";
 const Todos: React.FC<{items: Todo[]}> = (props)=> {
     return(
-        <ul>
+        <ul className={classes.todos}>
            {props.items.map((item) => (<TodoItem key={item.id} text={item.text}/>))}
         </ul>
     )
