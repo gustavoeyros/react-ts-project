2d5499ecfcffb0a22367b960d20ccf695063ed38
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-01-24 17:30:33 -0300

    feat: form and refs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import Todo from './models/todo'
 import Todos from './components/Todos'
 import './App.css'
 
 function App() {
-
+  const todos = [
+      new Todo('Learn React'),
+      new Todo('Learn Typescript')
+    ]
+  
 
   return (
-    <Todos items={['Learn React', 'Learn TypeScript']} />
+    <Todos items={todos} />
   )
 }
 
